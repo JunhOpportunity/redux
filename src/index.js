9902df5ea3d684e25be1492c4ad3a16cf4450b7b
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-12-09 18:57:59 +0900

    [feature] : Create redux 공부를 위한 예시 코드와 간단한 작동 구현

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+import { createStore } from "redux";
 
+const plus = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const reducer = (state = 0, action) => {
+  if (action.type === "PLUS") {
+    return state + 1;
+  } else if (action.type === "MINUS") {
+    return state - 1;
+  } else {
+    return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const onChange = () => {
+  number.innerText = store.getState();
+};
+
+store.subscribe(onChange);
+
+const handlePlus = () => {
+  store.dispatch({ type: "PLUS" });
+};
+
+const handleMinus = () => {
+  store.dispatch({ type: "MINUS" });
+};
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
