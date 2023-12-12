ffba0f9a4fe16782e8af7a83a02f23a318f74531
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-12-12 17:40:06 +0900

    [feat] : Add createReducer & configureStore

--- src/store.js
+++ src/store.js
@@ -1,18 +1,30 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 export const addToDo = createAction("ADD");
 export const deleteToDo = createAction("DELETE");
 
-const reducer = (state = ["test"], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now(), ...state }];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo !== action.payload);
-    default:
-      return state;
-  }
-};
+console.log(addToDo.type, deleteToDo.type);
 
-export const store = createStore(reducer);
+// const reducer = (state = ["test"], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now(), ...state }];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteToDo, (state, action) => {
+      state.filter((toDo) => toDo.id !== action.payload);
+    });
+});
+
+export const store = configureStore(reducer)
