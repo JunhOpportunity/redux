938c311c89aa4a910653c843008876d21e083eff
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-12-12 17:39:19 +0900

    [fix] : Fixed 모듈 에러 해결

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
   }
   function onSubmit(e) {
     e.preventDefault();
-    dispatch(addToDo.addToDo(text));
+    dispatch(addToDo(text));
     setText("");
   }
 
